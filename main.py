04994ee9638f6dbf90d6b80fad9e0f6b2a10c7ae
MexicanCacti <JosephMRDominguez@gmail.com>
2022-10-04 11:59:36 -0700

    Milo Dominguez

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+numbers = []
 
-# ******************************
-# Make your Code
-# ******************************
+for i in range(5):
+    numbers.append(int(input()))
+average = sum(numbers)/len(numbers)
+
+for i in range(len(numbers)):
+    if(numbers[i] > average):
+        print(numbers[i], end= " ")
